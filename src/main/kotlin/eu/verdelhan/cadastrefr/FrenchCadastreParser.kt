03c08307cc20bc8474d7b1428281809c4c573fc5
mdeverdelhan <marc.deverdelhan@yahoo.com>
2025-03-30 21:23:22 +0200

    Add javadoc

--- src/main/kotlin/eu/verdelhan/cadastrefr/FrenchCadastreParser.kt
+++ src/main/kotlin/eu/verdelhan/cadastrefr/FrenchCadastreParser.kt
@@ -1,30 +1,56 @@
-
 import eu.verdelhan.cadastrefr.model.BatimentFeatureCollection
 import eu.verdelhan.cadastrefr.model.ParcelleFeatureCollection
 import eu.verdelhan.cadastrefr.model.TsurfFeatureCollection
 import kotlinx.serialization.json.Json
 
 /**
- * Parser for French cadastre
+ * Parser for French cadastre JSON data.
+ * This class provides methods to parse different cadastral feature collections.
  */
 class FrenchCadastreParser {
 
+    /** JSON configuration */
     val json = Json {
         ignoreUnknownKeys = true
     }
 
+    /**
+     * Parses a JSON string into the specified type.
+     *
+     * @param T The type to parse into.
+     * @param jsonString The JSON string to parse.
+     * @return The parsed object of type T.
+     */
     inline fun <reified T> parseJson(jsonString: String): T {
         return json.decodeFromString(jsonString)
     }
 
+    /**
+     * Parses a JSON string into a [ParcelleFeatureCollection].
+     *
+     * @param jsonString The JSON string containing parcel data.
+     * @return A [ParcelleFeatureCollection] object.
+     */
     fun parseParcellesJson(jsonString: String): ParcelleFeatureCollection {
         return parseJson(jsonString)
     }
 
+    /**
+     * Parses a JSON string into a [BatimentFeatureCollection].
+     *
+     * @param jsonString The JSON string containing building data.
+     * @return A [BatimentFeatureCollection] object.
+     */
     fun parseBatimentsJson(jsonString: String): BatimentFeatureCollection {
         return parseJson(jsonString)
     }
 
+    /**
+     * Parses a JSON string into a [TsurfFeatureCollection].
+     *
+     * @param jsonString The JSON string containing topographic surface data.
+     * @return A [TsurfFeatureCollection] object.
+     */
     fun parsePciTsurfJson(jsonString: String): TsurfFeatureCollection {
         return parseJson(jsonString)
     }
